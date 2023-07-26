57e034a6d55f70072286f8338b7c07d534b03783
Ekaterina Myndresko <104676584+Catiska@users.noreply.github.com>
2023-07-26 17:39:48 +0300

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -4,8 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('DJANGO_KEY', 'secret_key')
-# DEBUG = os.environ.get('DEBUG', 'False')
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'foodgram.catiska.ru']
 
